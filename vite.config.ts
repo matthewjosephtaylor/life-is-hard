41af768b1f105fb0f692a39e1716f50b14a49377
Matt Taylor <matt@matthewjosephtaylor.com>
2024-09-13 16:11:56 -0500

    prod-pap-for-distribute

--- vite.config.ts
+++ vite.config.ts
@@ -28,18 +28,22 @@ export default defineConfig({
     __APP_VERSION__: JSON.stringify(version),
 
     // Local dev settings
-    __HOME_BASE__: JSON.stringify("http://localhost:8787"),
-    __PAP_ID__: JSON.stringify(
-      // "access-point-1725394696984-a760a179-851e-48d0-b276-85e7a426239c"
-      "access-point-1725753171369-a1fd9da6-1958-455b-a383-cc898840af79"
-    ),
+    // __HOME_BASE__: JSON.stringify("http://localhost:8787"),
+    // __PAP_ID__: JSON.stringify(
+    //   // "access-point-1725394696984-a760a179-851e-48d0-b276-85e7a426239c"
+    //   // GameMaster
+    //   "access-point-1725753171369-a1fd9da6-1958-455b-a383-cc898840af79"
+    // ),
 
     // Production settings
-    // __HOME_BASE__: JSON.stringify("https://ai-worker.intelligage.workers.dev"),
-    // __PAP_ID__: JSON.stringify(
-    //   // "access-point-1725571260862-23455f95-1253-4dfe-96e3-952ac6af647c"
-    //   "access-point-1725729203924-2cf4d59d-806b-4f4f-83fc-a6e086436ea0"
-    // ),
+    __HOME_BASE__: JSON.stringify("https://ai-worker.intelligage.workers.dev"),
+    __PAP_ID__: JSON.stringify(
+      // "access-point-1725571260862-23455f95-1253-4dfe-96e3-952ac6af647c"
+      // Pizza guy
+      // "access-point-1725729203924-2cf4d59d-806b-4f4f-83fc-a6e086436ea0"
+      // GameMaster
+      "access-point-1726260886551-b730b1b0-5b76-431b-bed9-bc8fa40d7f6e"
+    ),
     __APP_FRONT__: JSON.stringify("life-is-hard"),
   },
 });
